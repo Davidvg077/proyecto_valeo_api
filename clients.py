e82ff72e4869e7696ecfba74e57cf8c13e20649b
Omar David Valderrama <odvalderrama16@ucatolica.edu.co>
2025-10-21 16:31:28 -0500

    (clientes): agregamos endpoint POST para crear cliente

---
+++ clients.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlmodel import Session, select
+from database import get_session
+from models import Cliente
+from schemas import ClienteCreate
+
+router = APIRouter(prefix="/clientes", tags=["clientes"])
+
+@router.post("/", response_model=Cliente, status_code=201)
+def create_cliente(data: ClienteCreate, session: Session = Depends(get_session)):
+    cli = Cliente(**data.model_dump())
+    session.add(cli)
+    session.commit()
+    session.refresh(cli)
+    return cli
