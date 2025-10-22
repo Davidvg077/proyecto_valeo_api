07a5848bdf97328a349d8be2e0fe0945c775cf6b
Omar David Valderrama <odvalderrama16@ucatolica.edu.co>
2025-10-22 07:31:10 -0500

    (schemas) agregamos ProductoUpdate, ClienteCreate y ClienteUpdate

--- schemas.py
+++ schemas.py
@@ -1,8 +1,24 @@
 from typing import Optional
 from sqlmodel import SQLModel, Field
 
+# --- Productos ---
 class ProductoCreate(SQLModel):
     nombre: str = Field(min_length=2)
     precio: float = Field(gt=0)
     stock: int = Field(ge=0)
 
+class ProductoUpdate(SQLModel):
+    nombre: Optional[str] = Field(default=None, min_length=2)
+    precio: Optional[float] = Field(default=None, gt=0)
+    stock: Optional[int] = Field(default=None, ge=0)
+
+# --- Clientes ---
+class ClienteCreate(SQLModel):
+    nombre: str = Field(min_length=2)
+    ciudad: str
+    canal: str
+
+class ClienteUpdate(SQLModel):
+    nombre: Optional[str] = Field(default=None, min_length=2)
+    ciudad: Optional[str] = None
+    canal: Optional[str] = None
